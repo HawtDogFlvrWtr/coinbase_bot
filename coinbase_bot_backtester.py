5db76601f22b6c4a4172bbb8252f0193724b7914
Christopher Phipps <hawtdogflvrwtr@gmail.com>
2023-11-21 16:57:13 -0500

    Update coinbase_bot_backtester.py
    fixed a duplicate sell bug that actually lowered the backtesting amounts. need to update readme. Also changed this to store the run in memory instead of writing to disk as it's shorterm info anyway and can be identically replicated later.

--- coinbase_bot_backtester.py
+++ coinbase_bot_backtester.py
@@ -10,14 +10,15 @@ import ccxt
 import pandas as pd
 import pandas_ta as ta
 import time
+import uuid
 from prettytable import PrettyTable
 from tinydb import TinyDB, Query
+from tinydb.storages import MemoryStorage
 from threading import Thread
 from websocket import create_connection, WebSocketConnectionClosedException # websocket-client
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-o', '--orders_file', help="The json filename for the orders file", default='coinbase_bot_bt.json')
 parser.add_argument('-c', '--config_file', help="The json filename for the orders file", default='config.cfg')
 parser.add_argument('-s', '--start_time', help="The start time for backtesting", type=int, default=1672531200)
 parser.add_argument('-rb', '--rsi_buy_lt', help="The start time for backtesting", type=int, default=50)
@@ -26,7 +27,6 @@ parser.add_argument('-t', '--take_profit', help="The start time for backtesting"
 parser.add_argument('-sl', '--stoploss_percent', help="The start time for backtesting", type=int, default=10)
 
 args = parser.parse_args()
-orders_json_filename = args.orders_file
 config_file = args.config_file
 since_start = args.start_time
 rsi_buy_lt = args.rsi_buy_lt
@@ -53,8 +53,7 @@ current_prices = {}
 max_order_amount = buy_percent / 100 * spend_dollars
 max_orders = int(spend_dollars / max_order_amount)
 
-db = TinyDB(orders_json_filename, indent=4)
-db.truncate() # Blow away old backtesting data
+db = TinyDB(storage=MemoryStorage)
 Orders = Query()
 
 # Initialize the exchange and API keys
@@ -74,10 +73,10 @@ exchange = exchange_class({
 })
 
 def insert_order(status, symbol, buy_amount, buy_time, signal_time, buy_price):
-    db.insert({'symbol': symbol, 'status': status, 'buy_amount': buy_amount, 'buy_time': buy_time, 'signal_time': signal_time, 'buy_price': buy_price, 'sell_price': 0, 'sell_profit': 0, 'sell_time': 0})
+    db.insert({'order_id': uuid.uuid4(), 'symbol': symbol, 'status': status, 'buy_amount': buy_amount, 'buy_time': buy_time, 'signal_time': signal_time, 'buy_price': buy_price, 'sell_price': 0, 'sell_profit': 0, 'sell_time': 0})
 
-def update_order(timestamp, sell_price, sell_profit, sell_time):
-    db.update({'status': 'closed', 'sell_price': sell_price, 'sell_profit': sell_profit, 'sell_time': sell_time}, Orders.buy_time == timestamp)
+def update_order(order_id, sell_price, sell_profit, sell_time):
+    db.update({'status': 'closed', 'sell_price': sell_price, 'sell_profit': sell_profit, 'sell_time': sell_time}, Orders.order_id == order_id)
 
 def search_open_order(symbol):
     results = db.search((Orders.symbol == symbol) & (Orders.status == 'open'))
@@ -176,76 +175,64 @@ def main():
                 macd_last = df['MACD_12_26_9'].iloc[prev_index]
                 signal = row.MACDs_12_26_9
                 signal_last = df['MACDs_12_26_9'].iloc[prev_index]
-                close = row.close
+                current_price = row.close
                 rsi = row.RSI_14
-                last_timestamp = record_timestamp
                 note_timestamp = datetime.datetime.fromtimestamp(record_timestamp).strftime('%m-%d-%Y %H:%M:%S')
-                current_price = close
                 index += 1
                 # Buy Good Risk
                 #print("Symbol: %s MACD: %s MACDs %s LMACD: %s LMACDs %s RSI: %s" % (symbol, macd, signal, macd_last, signal_last, rsi))
                 if macd > signal and macd_last < signal_last and rsi <= rsi_buy_lt:
                     # Prevent duplicate coin
                     if allow_duplicates == 'False' and open_order_count(symbol) > 0: # Prevent duplicate coin
-                        #print('%s - Skipping buy of symbol %s because we already have an open order' % (note_timestamp, symbol))
                         continue
                     if open_order_count() >= max_orders: # Already met our max open orders
-                        #print('%s - Skipping buy of symbol %s because we are at our max orders.' % (note_timestamp, symbol))
                         continue
                     if buy_when_higher == 'False' and last_order_buy_price(symbol) > current_price: # Don't buy if we paid more for the last order
-                        #print('%s - Skipping buy of symbol %s because a previous buy was at a lower price.' % (note_timestamp, symbol))
                         continue
                     # DO BUY
                     buy_amount = max_order_amount / current_price
                     buy_time = last_timestamp
                     buy += 1
                     print('%s - Buying %s %s at %s.' % (note_timestamp, buy_amount, symbol, current_price))
-                    insert_order('open', symbol, buy_amount, buy_time, last_timestamp, current_price)
+                    insert_order('open', symbol, buy_amount, record_timestamp, record_timestamp, current_price)
+                    continue
                 # Sell Good Risk
-                elif macd < signal and macd_last > signal_last and rsi >= rsi_sell_gt:
-                    # DO SELL
-                    if not search_open_order(symbol):
-                        continue
-                    buy_orders = search_order(symbol)
-                    for buy_order in buy_orders:
-                        if buy_order['status'] == 'closed':
-                            continue
-                        timestamp = buy_order['buy_time']
-                        buy_price = buy_order['buy_price']
-                        buy_amount = buy_order['buy_amount']
-                        profit = round(((current_price - buy_price) / buy_price) * 100, 2)
-                        if profit < 0.5:
-                            continue
-                        profit_list.append(profit)
-                        sold += 1
-                        print('%s - Selling %s %s at %s. Profit: %s' % (note_timestamp, buy_amount, symbol, current_price, profit))
-                        update_order(timestamp, current_price, profit, last_timestamp)
                 buy_orders = search_order(symbol)
                 for buy_order in buy_orders:
-                    if buy_order['status'] == 'closed':
-                        continue
-                    timestamp = buy_order['buy_time']
+                    buy_time = buy_order['buy_time']
                     buy_price = buy_order['buy_price']
                     buy_amount = buy_order['buy_amount']
+                    order_id = buy_order['order_id']
                     profit = round(((current_price - buy_price) / buy_price) * 100, 2)
-    
-                    if profit <= stoploss_percent:
-                        profit_list.append(profit)
-                        stoploss += 1
-                        print('%s - STOPLOSS Selling %s %s at %s. Profit: %s' % (note_timestamp, buy_amount, symbol, current_price, profit))
-                        update_order(timestamp, current_price, profit, last_timestamp)
-                    elif profit >= take_profit:
-                        profit_list.append(profit)
-                        take_profit_count += 1
-                        print('%s - TAKEPROFIT Selling %s %s at %s. Profit: %s' % (note_timestamp, buy_amount, symbol, current_price, profit))
-                        update_order(timestamp, current_price, profit, last_timestamp)
+                    if buy_order['status'] == 'open':
+                        # Sell Signal
+                        if macd < signal and macd_last > signal_last and rsi >= rsi_sell_gt:
+                            print("HERE")
+                            if profit >= 0.5:
+                                profit_list.append(profit)
+                                sold += 1
+                                print('%s - Selling %s %s at %s. Profit: %s' % (note_timestamp, buy_amount, symbol, current_price, profit))
+                                update_order(order_id, current_price, profit, last_timestamp)
+                        # Stoploss
+                        elif profit <= stoploss_percent:
+                            profit_list.append(profit)
+                            stoploss += 1
+                            print('%s - STOPLOSS Selling %s %s at %s. Profit: %s' % (note_timestamp, buy_amount, symbol, current_price, profit))
+                            update_order(order_id, current_price, profit, last_timestamp)
+                        # Take Profit
+                        elif profit >= take_profit:
+                            profit_list.append(profit)
+                            take_profit_count += 1
+                            print('%s - TAKEPROFIT Selling %s %s at %s. Profit: %s' % (note_timestamp, buy_amount, symbol, current_price, profit))
+                            update_order(order_id, current_price, profit, last_timestamp)
+
                 if last_profit != sum(profit_list):
                     print("Profit on %s: %s%%" % (time_readable, round(sum(profit_list), 2)))
                     last_profit = sum(profit_list)
         since_start = int(since_start + sleep_lookup[timeframe])
         if since_start > time.time():
             print("Backtesting finished")
-            print("Final Profit %s%%" % (time_readable, round(sum(profit_list), 2)))
+            print("Final Profit %s%%" % round(sum(profit_list), 2))
             sys.exit(0)
 
 if __name__ == "__main__":
